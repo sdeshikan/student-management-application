45e55e8494dc3898da6c811c6fe38c3fce007b51
Sugumar Deshikan <sukumar.deshikan@gmail.com>
2022-07-03 21:19:35 +0530

    initial commit

---
+++ src/main/java/com/sdesh/sms/sb/t/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sdesh.sms.sb.t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
